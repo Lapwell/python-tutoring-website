4840368fa45b32f12e776bf86b0f3d3c616ca5a5
Byron.C <byronrwcor@protonmail.com>
2023-02-23 17:26:58 -0800

    got calendly ready for setting up sessions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,18 @@
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import {useState} from 'react'
-import {Navbar, SimpleGrid, Group, Button, CopyButton, Image, Container, Center, Popover, Space, Notification} from '@mantine/core'
+import {Navbar, SimpleGrid, Group, Button, CopyButton, Image, Container, Center, Popover, Space} from '@mantine/core'
 import {IconBrandTwitter} from '@tabler/icons'
-import {Calendar} from '@mantine/dates'
 
 export default function Home() {
-  const [value, setValue] = useState<Date[]>([]);
   return (
     <>
       <main className={styles.main}>
-        <Navbar height='4vh' width={'vw'}>
+        <Navbar height='6vh' width={{base:'vw'}}>
           <Group position='center'>
             <Navbar.Section><Link href='/'>Home</Link></Navbar.Section>
-            <Navbar.Section><Link href='#'>Schedule</Link></Navbar.Section>
+            <Navbar.Section><Link target='_blank' href='https://calendly.com/python-tutoring/python-tutoring?month=2023-02'>Schedule</Link></Navbar.Section>
             <Navbar.Section>
-              <Popover position='bottom' trapFocus>
+              <Popover position='bottom' withArrow>
                 <Popover.Target><Button>Contacts</Button></Popover.Target>
                 <Popover.Dropdown>
                   <Button component='a' target='_blank' href='https://twitter.com/Byron__Corbett'>Twitter</Button>
@@ -36,19 +33,25 @@ export default function Home() {
           <SimpleGrid cols={1}>
             <Container>
               <Center>
-                <h4>About myself</h4>
+                <h3>About myself</h3>
               </Center>
-              <p>I'm a young adult living in the Langford area, I am currently working for a local company that teaches kids about coding while I develop my skills.
-                 I have a passion for technology, science, games and sharing my interests. I am looking to teach teens about the Python coding language;
-                 helping them develop their skills, learn about coding and enjoy their passion to the fullest.</p>
+                <p>I'm a young adult living in the Langford area, I am currently working for a local company that teaches kids about coding while I develop my skills.
+                   I have a passion for technology, science, games, and sharing my interests. I am looking to teach teens about the Python coding language,
+                   helping them develop their skills, learn about coding, and enjoy their passion to the fullest.
+                 </p>
             </Container>
-            <Space h='10vh'/>
+            <Space h='8vh'/>
             <Container>
-              <Calendar  multiple value={value} onChange={setValue} />
+              <Center>
+                <h3>My Schedule</h3>
+              </Center>
+              <p>
+                I do session Friday-Monday 10am-2pm at coffee shops and libraries in the langford area for teens 15-19. Click Schedule to setup a session with me!
+              </p>
             </Container>
           </SimpleGrid>
         </Center>
       </main>
     </>
-  )
-}
+  );
+};
