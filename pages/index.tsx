398cda57d70ab2aada2601c73b0c4c78a467c0c9
Byron.C <byronrwcor@protonmail.com>
2023-03-09 13:28:25 -0800

    idr what i changed. stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import {Navbar, SimpleGrid, Group, Button, CopyButton, Image, Container, Center, Popover, Space} from '@mantine/core'
+import {Navbar, SimpleGrid, Group, Button, CopyButton, Image, Container, Center, Popover, Space, Text} from '@mantine/core'
 import {IconBrandTwitter} from '@tabler/icons'
 
 export default function Home() {
@@ -9,7 +9,6 @@ export default function Home() {
       <main className={styles.main}>
         <Navbar height='6vh' width={{base:'vw'}}>
           <Group position='center'>
-            <Navbar.Section><Link href='/'>Home</Link></Navbar.Section>
             <Navbar.Section><Link target='_blank' href='https://calendly.com/python-tutoring/python-tutoring?month=2023-02'>Schedule</Link></Navbar.Section>
             <Navbar.Section>
               <Popover position='bottom' withArrow>
@@ -31,23 +30,23 @@ export default function Home() {
         <Space h='4vh' />
         <Center>
           <SimpleGrid cols={1}>
-            <Container>
+            <Container size='sm'>
               <Center>
                 <h3>About myself</h3>
               </Center>
-                <p>I'm a young adult living in the Langford area, I am currently working for a local company that teaches kids about coding while I develop my skills.
-                   I have a passion for technology, science, games, and sharing my interests. I am looking to teach teens about the Python coding language,
-                   helping them develop their skills, learn about coding, and enjoy their passion to the fullest.
-                 </p>
+              <Text ta='center'>I'm a young adult living in the Langford area, I am currently working for a local company that teaches kids about coding while I develop my skills.
+                I have a passion for technology, science, games, and sharing my interests. I am looking to teach teens about the Python coding language,
+                helping them develop their skills, learn about coding, and enjoy their passion to the fullest.
+              </Text>
             </Container>
             <Space h='8vh'/>
-            <Container>
+            <Container size='xs'>
               <Center>
                 <h3>My Schedule</h3>
               </Center>
-              <p>
-                I do session Friday-Monday 10am-2pm at coffee shops and libraries in the langford area for teens 15-19. Click Schedule to setup a session with me!
-              </p>
+              <Text ta="center">
+                I do session Friday-Monday in coffee shops and libraries in the Langford area. My regular hours are 10am-1pm.
+              </Text>
             </Container>
           </SimpleGrid>
         </Center>
