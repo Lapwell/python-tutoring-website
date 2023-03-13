b5e543ad9d3d6150772017821faf6885f1689010
Byron.C <byronrwcor@protonmail.com>
2023-03-13 06:51:12 -0700

    name changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Langford Coding Tutor</title>
+        <title>Python Coding Tutor</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <MantineProvider
