587a99d80886f3e443101ef97caad48e092ae875
Byron.C <byronrwcor@protonmail.com>
2023-03-13 06:37:42 -0700

    Got the layout looking much better. Got to figure out how to get the picture to have the text wrap around it at some point. Pretty it up some more, integrate calendly in some way, show off some projects I have finished for possible customers to see.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Python Tutoring</title>
+        <title>Langford Coding Tutor</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <MantineProvider
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import {Navbar, SimpleGrid, Group, Button, CopyButton, Image, Container, Center, Popover, Space, Text} from '@mantine/core'
+import {Navbar, SimpleGrid, Group, Button, CopyButton, Image, Container, Center, Popover, Space, Text, BackgroundImage} from '@mantine/core'
 import {IconBrandTwitter} from '@tabler/icons'
+
+//https://calendly.com/byronrwc/code-tutoring
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <Navbar height='6vh' width={{base:'vw'}}>
+        <Navbar height='8vh' width={{base:'vw'}}>
           <Group position='center'>
-            <Navbar.Section><Link target='_blank' href='https://calendly.com/python-tutoring/python-tutoring?month=2023-02'>Schedule</Link></Navbar.Section>
+            <Navbar.Section><Link target='_blank' href='https://calendly.com/byronrwc/code-tutoring'>Schedule</Link></Navbar.Section>
             <Navbar.Section>
               <Popover position='bottom' withArrow>
                 <Popover.Target><Button>Contacts</Button></Popover.Target>
@@ -18,7 +20,7 @@ export default function Home() {
                   <CopyButton value='byronc@pythontutoring.ca'>
                     {({copied, copy}) => (
                       <Button color={copied ? 'teal' : 'blue'} onClick={copy}>
-                        {copied ? 'Copied Email' : 'Copy Email'}
+                        {copied ? 'Copied' : 'Copy Email'}
                       </Button>
                     )}
                   </CopyButton>
@@ -27,29 +29,36 @@ export default function Home() {
             </Navbar.Section>
           </Group>
         </Navbar>
-        <Space h='4vh' />
+        <Space h='16vh' />
+
         <Center>
-          <SimpleGrid cols={1}>
-            <Container size='sm'>
-              <Center>
-                <h3>About myself</h3>
-              </Center>
-              <Text ta='center'>I'm a young adult living in the Langford area, I am currently working for a local company that teaches kids about coding while I develop my skills.
-                I have a passion for technology, science, games, and sharing my interests. I am looking to teach teens about the Python coding language,
-                helping them develop their skills, learn about coding, and enjoy their passion to the fullest.
+          <SimpleGrid cols={2}>
+            <Container>
+              <h3>About My Service</h3>
+              <Text>
+                My service is about helping you learn the basics and fundamentals of coding in a chill, relaxed environment.
+                What are variables? What are functions? What the heck is a Class!? I will help you learn what these are through fun little projects using Python!
+                <br/><br/>
+                Python is an easy to read and write coding language, it's basically English! What you learn with me using Python can be applied
+                to any other language you learn in the future, giving you a solid boost to your future with coding.
+                <br/><br/>
+                No previous experience required. You will need a your own laptop.
               </Text>
             </Container>
-            <Space h='8vh'/>
-            <Container size='xs'>
-              <Center>
-                <h3>My Schedule</h3>
-              </Center>
-              <Text ta="center">
-                I do session Friday-Monday in coffee shops and libraries in the Langford area. My regular hours are 10am-1pm.
+            <Container>
+              <h3>About Myself</h3>
+              <Image src="temp-profile.jpg" alt="Photo of me" width={100} height={100}/>
+              <Text>
+                Hello, I'm Byron! I'm a local, born here on the island in the Westshore area. I work for a local tech company that teaches kids programming and STEM
+                while I pursue education and personal goals.
+                <br/> <br/>
+                My passion and interests are in computers, science and games. I have been self-teaching coding for about 2 years now.
+                I look forward to helping others start their journey with programming!
               </Text>
             </Container>
           </SimpleGrid>
         </Center>
+
       </main>
     </>
   );
